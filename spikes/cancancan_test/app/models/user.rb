b6f1202463cd81286d30b45332ae140eaff48766
Iain MacQuarrie <iainc.macquarrie@gmail.com>
2015-04-26 14:45:49 +0000

    Finished the authorization and authentication spikes

---
+++ spikes/cancancan_test/app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+ has_many :user_roles, :dependent => :destroy
+ has_many :roles, :through => :user_roles
+  
+  def has_role?(name)
+    roles.pluck(:name).member?(name.to_s)
+  end
+end
