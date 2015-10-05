e40960a4c3445e383d14c3876fc2e586e4e0fd6d
Iain MacQuarrie <iainc.macquarrie@gmail.com>
2015-10-05 04:49:53 +0000

    added new roles to the seed file along with two other users to test the roles. Non-admins all have the same permissions atm

--- project/gigapan-ed/db/seeds.rb
+++ project/gigapan-ed/db/seeds.rb
@@ -6,6 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 admin = Role.create(name:"admin")
+teacher = Role.create(name:"teacher")
+student = Role.create(name:"student")
 # password and confirmation have to be 8 characters or longer or this will not create a user
 User.create(username:"batman",password:"password",password_confirmation:"password",email:"batman@gothamcity.net") 
 UserRole.create(user_id: User.first.id,role_id: Role.first.id)  
+User.create(username:"robin",password:"password",password_confirmation:"password",email:"robin@gothamcity.net") 
+UserRole.create(user_id: User.second.id,role_id: Role.second.id)  
+User.create(username:"bruce",password:"password",password_confirmation:"password",email:"bruce@gothamcity.net") 
+UserRole.create(user_id: User.third.id,role_id: Role.third.id)  
