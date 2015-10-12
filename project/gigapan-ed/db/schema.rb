654d0c79fb50616d84d38a01fd1115f9b26a71ff
Mo Morgan <momorgan7@gmail.com>
2015-10-12 20:20:45 +0000

    added users avatars

--- project/gigapan-ed/db/schema.rb
+++ project/gigapan-ed/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612034056) do
+ActiveRecord::Schema.define(version: 20151012193000) do
 
   create_table "roles", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150612034056) do
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
     t.string   "username",               limit: 255
+    t.string   "avatar",                 limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
