b9fd9f0b588befd23e3de2ecae37088c1a6c2750
Iain MacQuarrie <iainc.macquarrie@gmail.com>
2015-10-21 02:20:17 +0000

    small fixes to edit user profile and stuff

--- project/gigapan-ed/app/models/role.rb
+++ project/gigapan-ed/app/models/role.rb
@@ -3,4 +3,9 @@ class Role < ActiveRecord::Base
   has_many :users, :through => :user_roles
   
   RailsAdmin.config {|c| c.label_methods << :name}
+  
+  validates :name,
+  :presence => true,
+  :uniqueness => { :case_sensitive => false}, # etc.
+  length: { maximum: 50 }
 end
