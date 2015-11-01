d52887db606ec90360e641f347571f21b652ceea
Iain MacQuarrie <iainc.macquarrie@gmail.com>
2015-11-01 22:04:34 +0000

    fixes to user show page to account for new models. Adds current user to project on project create. Validations for org and project.

--- project/gigapan-ed/app/models/organization.rb
+++ project/gigapan-ed/app/models/organization.rb
@@ -1,4 +1,10 @@
 class Organization < ActiveRecord::Base
     has_many :users
     accepts_nested_attributes_for :users
+    validates :city,
+    :presence => true
+    validates :state,
+    :presence => true
+    validates :country,
+    :presence => true
 end
