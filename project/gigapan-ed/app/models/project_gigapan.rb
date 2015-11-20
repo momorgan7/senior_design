d14081d1f7b7b04ae4284fea381fe3e35c8807bd
Mo Morgan <momorgan7@gmail.com>
2015-11-20 21:07:11 +0000

    new and show actions for project_gigapans

--- project/gigapan-ed/app/models/project_gigapan.rb
+++ project/gigapan-ed/app/models/project_gigapan.rb
@@ -1,18 +1,14 @@
 class ProjectGigapan < ActiveRecord::Base
     belongs_to :project
     has_many :comments
+    attr_accessor :url
+
+  def url
+    @url
+  end
+
     
-    validates :project_id,
-    :presence => true
   
-    validates :ext_id,
-    :presence => true
-    validates :authcode,
-    :presence => true
-    validates :width,
-    :presence => true
-    validates :height,
-    :presence => true  
     validates :name,
     :presence => true,
     length: { minimum: 6 }
