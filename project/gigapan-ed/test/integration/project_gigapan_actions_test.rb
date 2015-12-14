33f2bb28333d6a1b5e020087b903d311d8b4b594
Mo Morgan <momorgan7@gmail.com>
2015-12-14 01:11:44 +0000

    formating

--- project/gigapan-ed/test/integration/project_gigapan_actions_test.rb
+++ project/gigapan-ed/test/integration/project_gigapan_actions_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ProjectGigapanActionsTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:two)
+    @user1 = users(:one)
     @project = projects(:one)
     @gigapan = project_gigapans(:one)
   end
@@ -35,4 +36,21 @@ class ProjectGigapanActionsTest < ActionDispatch::IntegrationTest
     post_via_redirect project_gigapans_path, 'project_gigapan[name]' => 'new name', 'project_gigapan[desc]' => 'new description', 'project_gigapan[url]' => 'http://gigapan.com/gigapans/1048', 'project_gigapan[project_id]' => @project.id
     assert_template 'project_gigapans/show'
   end
+  
+    test "edit gigapan admin" do
+    get login_path
+    post_via_redirect user_session_path, 'user[username]' => @user1.username, 'user[password]' =>  'password'
+    get edit_project_gigapan_path(@gigapan)
+   patch_via_redirect project_gigapan_path(@gigapan), 'project_gigapan[name]' => 'new name', 'project_gigapan[desc]' => 'new description'
+    assert_template 'project_gigapans/show'
+  end
+  
+  test "create gigapan admin" do
+    get login_path
+    post_via_redirect user_session_path, 'user[username]' => @user1.username, 'user[password]' =>  'password'
+    get new_project_gigapan_path(:project_id => @project.id)
+    post_via_redirect project_gigapans_path, 'project_gigapan[name]' => 'new name', 'project_gigapan[desc]' => 'new description', 'project_gigapan[url]' => 'http://gigapan.com/gigapans/645', 'project_gigapan[project_id]' => @project.id
+    assert_template 'project_gigapans/show'
+  end
+  
 end
